a1c5fc475a17849dc4714276843648d44c9e18b8
InfantryMan <rust-skate@mail.ru>
2021-04-14 16:22:50 +0300

    [+] show article

--- blog/apps/myblog/urls.py
+++ blog/apps/myblog/urls.py
@@ -2,7 +2,9 @@ from django.urls import path
 
 from . import views
 
+app_name='myblog'
 urlpatterns = [
     path('', views.index, name = 'index'),
-    
+    path('<int:article_id>/', views.detail, name = 'detail')
+
 ]
--- blog/apps/myblog/views.py
+++ blog/apps/myblog/views.py
@@ -1,5 +1,17 @@
+from django.http import Http404, HttpResponseRedirect
 from django.shortcuts import render
-from django.http import HttpResponse
+
+from .models import Article, Comment
 
 def index(request):
-    return HttpResponse("Привет, мир!")
+    latest_articles_list = Article.objects.order_by('-pub_date')[:5]
+    return render(request, 'myblog/list.html', {'latest_articles_list' : latest_articles_list})
+
+def detail(request, article_id):
+    try:
+        a = Article.objects.get(id = article_id)
+        print(a)
+    except:
+        raise Http404('Статья не найдена!')
+
+    return render(request, 'myblog/detail.html', {'article': a})
